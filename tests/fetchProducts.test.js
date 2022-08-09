8184f0968e080eadae8048345441305eaed9dcb4
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-08-09 17:29:25 -0300

    requisito 1 resolvido

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,23 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  test('Teste se fetchProducts é uma função', ()=>{
+    expect(typeof fetchProducts).toBe('function')
+  })
+  test('Teste se fetchProducts é chamada ao receber o argumento computador', async ()=>{
+    await fetchProducts('computador')
+    expect(fetch).toBeCalled()
+  })
+  test('Teste se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint https://api.mercadolibre.com/sites/MLB/search?q=computador', async ()=>{
+    await fetchProducts('computador')
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  })
+  test('Teste se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch', async ()=>{
+    expect(await fetchProducts('computador')).toEqual(computadorSearch)    
+  })
+  test('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async ()=>{
+    await fetchProducts()
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'));    
+  })
 });
