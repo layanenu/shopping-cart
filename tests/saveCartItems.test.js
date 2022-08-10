174755484a0af7a105d547fa9a18c25d82a7226b
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-08-10 19:26:39 -0300

    requisito 6 e 7 resolvido

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,13 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  test('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', async ()=>{
+    await saveCartItems('<ol><li>Item</li></ol>')
+    expect(fetch).toBeCalled('localStorage.setItem')
+  })
+  test('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros', async ()=>{
+    await saveCartItems('<ol><li>Item</li></ol>')
+    expect(fetch).toBeCalled(localStorage.setItem(cartItems, argumento))
+  })
 });
